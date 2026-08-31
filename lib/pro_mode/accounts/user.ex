29f01f2430069d6ba559f18680cce5f0d1cc8958
Logan <natan005@proton.me>
2026-08-31 15:05:25 -0300

    feat: add user changeset

--- lib/pro_mode/accounts/user.ex
+++ lib/pro_mode/accounts/user.ex
@@ -1,5 +1,6 @@
 defmodule ProMode.Accounts.User do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     field :name, :string
@@ -8,5 +9,12 @@ defmodule ProMode.Accounts.User do
     field :total_score, :integer
     belongs_to :role, ProMode.Accounts.Role
     has_many :habit_completions, ProMode.Habits.HabitCompletion
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :lastname, :telegram_id])
+    |> validate_required([:name, :telegram_id])
+    |> unique_constraint(:telegram_id)
   end
 end
